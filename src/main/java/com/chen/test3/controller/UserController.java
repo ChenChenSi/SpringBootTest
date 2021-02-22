5e29f8556c836890683f6652c8dc9279b7188d29
names <49894120+ChenChenSi@users.noreply.github.com>
2021-02-23 02:31:37 +0800

    测试提交不同分支

--- src/main/java/com/chen/test3/controller/UserController.java
+++ src/main/java/com/chen/test3/controller/UserController.java
@@ -19,6 +19,7 @@ public class UserController {
 
     @GetMapping("/users")
     public ModelAndView users(){
+        //创建数据源
         List<User> users = new ArrayList<>();
         User u1 = new User("1","小明1",
                 "123456","1234567890123","111","222");
@@ -36,8 +37,11 @@ public class UserController {
         users.add(u2);
         users.add(u3);
         users.add(u4);
-
+        //23-39行：构建返回数据
+        //43-47行：创建返回ModelAndView，并设置视图名为users，返回数据为所创建的List集合
+        //创建视图
         ModelAndView mv = new ModelAndView();
+        //装填视图：路径、数据
         mv.addObject("users",users);
         mv.setViewName("users");
         return mv;
